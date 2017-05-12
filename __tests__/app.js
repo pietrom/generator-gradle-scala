bf1114ed73002e672687532f079bc13ed8a08aab
Pietro Martinelli <amicofragile@gmail.com>
2017-05-12 21:34:14 +0200

    TEST Added more tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -2,11 +2,11 @@
 const path = require('path')
 const assert = require('yeoman-assert')
 const helpers = require('yeoman-test')
+const prompts = { name: 'test-sample', groupId: 'com.github.example' }
 
 describe('generator-gradle-scala:app', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({name: 'test-sample'})
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts(prompts)
   })
 
   it('creates .yo-rc.json', () => {
@@ -23,5 +23,13 @@ describe('generator-gradle-scala:app', () => {
 
   it('uses name from prompt in settings.gradle', () => {
     assert.fileContent('settings.gradle', 'rootProject.name = test-sample')
+  })
+
+  it('uses groupId from prompt in build.gradle', () => {
+    assert.fileContent('build.gradle', `group = '${prompts.groupId}'`)
+  })
+
+  it('build.gradle contains fixed version', () => {
+    assert.fileContent('build.gradle', 'version = \'1.0.0-SNAPSHOT\'')
   })
 })
