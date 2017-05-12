1962d69db53d4fed3c9e28ef07299585968fb850
Pietro Martinelli <amicofragile@gmail.com>
2017-05-12 21:25:21 +0200

    STYLE No more semicolons anywhere (and eslint forces this)

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,20 +1,20 @@
-'use strict';
-const path = require('path');
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const mkdirp = require('mkdirp');
+'use strict'
+const path = require('path')
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
+const mkdirp = require('mkdirp')
 
 module.exports = class extends Generator {
   constructor(args, opts) {
-    super(args, opts);
-    this.argument('appname', {type: String, required: false});
+    super(args, opts)
+    this.argument('appname', {type: String, required: false})
   }
 
   prompting() {
     this.log(yosay(
       'Welcome to the primo ' + chalk.red('generator-gradle-scala') + ' generator!'
-    ));
+    ))
 
     const prompts = [{
       type: 'input',
@@ -41,25 +41,25 @@ module.exports = class extends Generator {
       name: 'junitVersion',
       message: 'JUnit version to use',
       default: '4.12'
-    }];
+    }]
 
     return this.prompt(prompts).then(props => {
-      this.props = props;
-    });
+      this.props = props
+    })
   }
   default() {
     if (path.basename(this.destinationPath()) !== this.props.name) {
       this.log(
         'Your project must be inside a folder named ' + this.props.name + '\n' +
         'I\'ll automatically create this folder.'
-      );
-      mkdirp(this.props.name);
-      this.destinationRoot(this.destinationPath(this.props.name));
+      )
+      mkdirp(this.props.name)
+      this.destinationRoot(this.destinationPath(this.props.name))
     }
   }
 
   writing() {
-    this.fs.copyTpl(this.templatePath('build.gradle'), this.destinationPath('build.gradle'), this.props);
-    this.fs.copyTpl(this.templatePath('settings.gradle'), this.destinationPath('settings.gradle'), this.props);
+    this.fs.copyTpl(this.templatePath('build.gradle'), this.destinationPath('build.gradle'), this.props)
+    this.fs.copyTpl(this.templatePath('settings.gradle'), this.destinationPath('settings.gradle'), this.props)
   }
-};
+}
